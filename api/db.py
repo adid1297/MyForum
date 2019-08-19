28cc904548381d00b7e395fac29f0763bbb90171
Diego <adid1297@gmail.com>
2019-08-19 12:02:35 +0800

    Name db params

--- api/db.py
+++ api/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine("postgresql://postgres@db/postgres")
+engine = create_engine("postgresql://dbuser:dbpass@db/dbmain")
 session = scoped_session(sessionmaker(bind=engine))
