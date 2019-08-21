d9f64eaa607a7bbf067e6fc57357baef4604811e
Diego <adid1297@gmail.com>
2019-08-21 10:52:49 +0800

    Setup template for feed page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Landing from './components/Landing';
+import Feed from './components/Feed';
 
 function App() {
   return (
     <div>
-      <Landing />
+      <Feed />
     </div>
   );
 }
